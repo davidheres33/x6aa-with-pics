868496048c73e86b5be8d00a707a05884e8dcd27
davidheres33 <itsaax@gmail.com>
2025-07-27 06:40:11 -0500

    Update script.js

--- purchase-eb415ae7-7b5f-4de7-9d43-952c4a978620/script.js
+++ purchase-eb415ae7-7b5f-4de7-9d43-952c4a978620/script.js
@@ -85,7 +85,7 @@ document.addEventListener('DOMContentLoaded', () => {
             '100000': '100,000 Followers'
         },
         gamerscore: {
-            '50000': 'a*a Gamertag',
+            '50000': 'Purchases Gamertag',
             '200000': '200,000 GS',
             '500000': '500,000 GS',
             '1000000': '1,000,000 GS'
