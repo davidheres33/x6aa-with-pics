670629fe1c0c495f586ae91f600b0c87a0b2b512
davidheres33 <itsaax@gmail.com>
2025-08-14 08:03:25 -0500

    Update script.js

--- purchase-eb15ae7-7b5f-4de7-9d43-952c4a978620/script.js
+++ purchase-eb15ae7-7b5f-4de7-9d43-952c4a978620/script.js
@@ -85,7 +85,7 @@ document.addEventListener('DOMContentLoaded', () => {
             '100000': '100,000 Followers'
         },
         gamerscore: {
-            '50000': 'Custom LFG Game',
+            '50000': 'Xbox kkpd',
             '200000': '200,000 GS',
             '500000': '500,000 GS',
             '1000000': '1,000,000 GS'
